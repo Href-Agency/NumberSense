99907adc7c5ea0ad54a0f2b6c90282caf727c261
Dale HREF <dale@hrefagency.co.uk>
2021-04-23 14:05:12 +0100

    updated meta tag and scripts file

---
+++ Analytics.js
@@ -0,0 +1,7 @@
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src=""></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());  gtag('config', 'UA-128755175-4');
+</script>
